857d5b5a13147241403e628ab9cfee928779eaeb
dakingha69 <dongha.kim210@gmail.com>
2018-10-27 18:54:44 +0200

    Ad clone cleanup to Home.js

--- client/src/components/PopUpChallenge.js
+++ client/src/components/PopUpChallenge.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-import { Box, Layer } from 'grommet';
+import React from 'react'
+import { Box, Layer, Heading } from 'grommet'
+
+import RoboList from './RoboList'
 
 const PopUpChallenge = (props) => (
-    <Layer margin="large" full= 'true' onClickOutside={() => {props.onClose()}} onEsc={() => {props.onClose()}}>
-        <Box overflow="auto" border={{ color: 'brand', size: 'large' }} pad='xlarge' elevation = 'medium' basis= 'xlarge'>
-              <Box pad="xlarge">Challenge List</Box>
-        </Box>
-    </Layer>);
+  <Layer
+    margin='large'
+    onClickOutside={() => props.onClose()}
+    onEsc={() => props.onClose()}
+  >
+    <Box
+      overflow='auto'
+      border={{ color: 'brand', size: 'large' }}
+      pad='medium'
+      elevation='medium'
+    >
+      <Heading size='small'>
+        Select a opponent
+      </Heading>
+      <RoboList robots={props.robots} />
+    </Box>
+  </Layer>
+)
 
-export default PopUpChallenge;
+export default PopUpChallenge
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,23 +3,39 @@ import { Box, Heading, Layer } from 'grommet';
 import Header from '../components/Header';
 import RobotCard from '../components/RobotCard';
 import PopUpChallenge from '../components/PopUpChallenge';
-import PopUpGive from '../components/PopUpGive';
+import PopUpGive from '../components/PopUpGive'
 
- 
-class Home extends React.Component {
+class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { isChallenge: false, isGive: false, robots: [{name: 'Robo 1', id: 1}, {name: 'Robo 2', id: 2}, {name: 'Robo 3', id: 3}, {name: 'Robo 4', id: 4}]};
+    this.state = {
+      isSelectOpponentModalOpen: false,
+      isGiveModalOpen: false,
+      robots: [
+        { name: 'Robo 1', id: 1, owner: 'Owner 1' },
+        { name: 'Robo 2', id: 2, owner: 'Owner 2' },
+        { name: 'Robo 3', id: 3, owner: 'Owner 3' },
+        { name: 'Robo 4', id: 4, owner: 'Owner 4' }
+      ]
+    }
   }
 
   _challenge(id) {
-    this.setState({isChallenge: true});
+    this.setState({ isSelectOpponentModalOpen: true })
   }
+
   _give(robo) {
-    this.setState({isGive: true, robo});
-    
+    this.setState({
+      isGiveModalOpen: true,
+      robo
+    })
   }
-  _close() {this.setState({isChallenge: false, isGive: false});
+
+  _close() {
+    this.setState({
+      isSelectOpponentModalOpen: false,
+      isGiveModalOpen: false
+    })
   }
 
   render() {
@@ -30,12 +46,28 @@ class Home extends React.Component {
             <Heading level="3">
               Your Robos
             </Heading>
-              <Box direction= 'row-responsive' wrap= 'true'>
-                {this.state.robots.map(robot=><RobotCard robot={robot} onClickChallenge={this._challenge.bind(this)} onClickGive={this._give.bind(this)}/>)}
-              </Box>
+            <Box direction= 'row-responsive' wrap= 'true'>
+              {this.state.robots.map(robot=> (
+                <RobotCard
+                  robot={robot}
+                  onClickChallenge={this._challenge.bind(this)}
+                  onClickGive={this._give.bind(this)}
+                />
+              ))}
+            </Box>
           </Box>
-            {this.state.isChallenge ? (<PopUpChallenge onClose={this._close.bind(this)} />) : null}
-            {this.state.isGive ? (<PopUpGive robo={this.state.robo} onClose={this._close.bind(this)} />) : null}
+          {this.state.isSelectOpponentModalOpen && (
+            <PopUpChallenge
+              onClose={this._close.bind(this)}
+              robots={this.state.robots}
+            />
+          )}
+          {this.state.isGiveModalOpen && (
+            <PopUpGive
+              robo={this.state.robo}
+              onClose={this._close.bind(this)}
+            />
+          )}
 
       </Box>
     )
